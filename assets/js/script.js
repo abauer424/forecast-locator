58954798e9d2327df40c3d9dc11c9f97d40e44e0
Amber Bauer <amberbauer@Ambers-MacBook-Pro.local>
2022-03-08 16:48:20 -0500

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,34 @@
 var APIKey = "46161b42a7f28e8be0a54d1ce4a7253e";
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
+var cityContainerEl = document.querySelector("#city-container");
+var citySearchTerm = document.querySelector("#city-search-term");
 
 
 var displayWeather = function(city, searchTerm) {
-    console.log(city);
-    console.log(searchTerm);
+   cityContainerEl.textContent = "";
+   citySearchTerm.textContent = searchTerm; 
 }
 
 
 $(document).ready(function () {
     $("#search").on("click", function () {
         var city = $("#city").val().trim()
-        console.log(city);
         getCityCoords(city);
     })
     var getCityCoords = function (cityTerm) {
-        console.log(cityTerm)
         fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityTerm + "&appid=" + APIKey).then(function (response) {
             response.json().then(function (data) {
                 displayWeather(data, city)
                 if (response.ok) {
-                    console.log(response);
                 } else {
                     alert('Error: Please enter a valid city name')
                 }
-                console.log(data);
                 var lat = data[0].lat
                 var lon = data[0].lon
                 getCityWeather(lat, lon);
             })
         });
-        console.log("outside");
     };
 
     var getCityWeather = function (lat, lon) {
@@ -47,7 +44,6 @@ $(document).ready(function () {
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
-    console.log(event);
 };
 
 
