dbd9c5e753b6253cb14e39078123b7b67b4d3448
Amber Bauer <amberbauer@Ambers-MacBook-Pro.local>
2022-03-08 16:28:58 -0500

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 var APIKey = "46161b42a7f28e8be0a54d1ce4a7253e";
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
+
+
+var displayWeather = function(city, searchTerm) {
+    console.log(city);
+    console.log(searchTerm);
+}
+
+
 $(document).ready(function () {
     $("#search").on("click", function () {
         var city = $("#city").val().trim()
@@ -9,6 +19,12 @@ $(document).ready(function () {
         console.log(cityTerm)
         fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityTerm + "&appid=" + APIKey).then(function (response) {
             response.json().then(function (data) {
+                displayWeather(data, city)
+                if (response.ok) {
+                    console.log(response);
+                } else {
+                    alert('Error: Please enter a valid city name')
+                }
                 console.log(data);
                 var lat = data[0].lat
                 var lon = data[0].lon
@@ -29,8 +45,19 @@ $(document).ready(function () {
 }
 )
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+};
 
 
+userFormEl.addEventListener("submit", formSubmitHandler);
 
+var city = cityInputEl.value.trim();
 
+if (city) {
+    cityInputEl.value = "";
+} else[
+    alert("Please enter a valid city name")
+]
 
